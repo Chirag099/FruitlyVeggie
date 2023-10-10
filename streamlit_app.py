4652993dd33820d81cfe44cb3e963c50d92a91b9
Chirag <thakur.chirag099@gmail.com>
2023-10-11 00:15:17 +0530

    Upgraded Training Model

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #Tensorflow Model Prediction
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("training_model (1).h5")
+    model = tf.keras.models.load_model("training_model.h5")
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(64,64))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
